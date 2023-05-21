8cd4abf0d7bab03289d69e0008cce387d66a9e38
AndreiPrisa <andrei.prisacaru680@gmail.com>
2023-05-21 17:51:43 +0300

    Add user profile dropdown

--- pages/search/index.js
+++ pages/search/index.js
@@ -6,7 +6,7 @@ import { getUserProps } from '../../utils/auth';
 export default function Home(props) {
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1);
-  const resultsPerPage = 9; // Number of results to display per page
+  const resultsPerPage = 6; // Number of results to display per page
 
   function searchHandler(searchData) {
     router.push({
@@ -32,7 +32,7 @@ export default function Home(props) {
   const paginationLinks = [];
   for (let i = 1; i <= pageNumbers; i++) {
     paginationLinks.push(
-      <button key={i} onClick={() => handlePageChange(i)}>
+      <button class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-2.5 py-1 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" key={i} onClick={() => handlePageChange(i)}>
         {i}
       </button>
     );
@@ -42,20 +42,22 @@ export default function Home(props) {
     <>
       <h2>OUR Search Engine results</h2>
       <NewSearch onSearch={searchHandler} title="Search again" />
+      <div class="space-y-2">
       <div>
         {currentResults.map((searchResult, index) => {
           return (
             <div key={index}>
               <h3>
-                <a target="_blank" href={`${searchResult._source.original_url}`} rel="noopener noreferrer">{searchResult._source.original_url}</a>
+                <a class="block w-full text-center p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" target="_blank" href={`${searchResult._source.original_url}`} rel="noopener noreferrer">{searchResult._source.original_url}</a>
               </h3>
               <hr />
             </div>
           )
         })}
       </div>
-      <div>
+      <div class="text-center">
         {paginationLinks}
+      </div>
       </div>
     </>
   )
